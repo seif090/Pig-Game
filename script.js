c691fe9ce9773ed7c3ade1c06b9bfa2f92a79745
seif <seiftarek090@gmail.com>
2025-05-05 17:53:54 +0300

    Starting the project by selecting elements and making starting conditions

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+// Selecting elements
+const score0 = document.querySelector('#score--0');
+const score1 = document.getElementById('score--1');
+const dice = document.querySelector('.dice');
+
+// Starting conditions
+score0.textContent = 0;
+score1.textContent = 0;
+dice.classList.add('hidden');
+
